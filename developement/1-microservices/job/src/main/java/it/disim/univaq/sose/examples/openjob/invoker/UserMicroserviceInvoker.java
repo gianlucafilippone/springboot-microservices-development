866c6bbc0fc6e6c390ca56a2ef738e2335227890
Gianluca Filippone <filgianluca93@gmail.com>
2024-05-17 12:47:03 +0200

    Added first development phase

---
+++ developement/1-microservices/job/src/main/java/it/disim/univaq/sose/examples/openjob/invoker/UserMicroserviceInvoker.java
@@ -0,0 +1,37 @@
+package it.disim.univaq.sose.examples.openjob.invoker;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+@Component
+public class UserMicroserviceInvoker {
+
+	public static final String FIELD_ID = "id";
+	public static final String FIELD_USERNAME = "username";
+	public static final String FILD_PASSWORD = "password";
+	public static final String FIELD_PASSWORD_EXPIRED = "passwordExpired";
+	public static final String FIELD_ACTIVE_USER = "active";
+	public static final String FIELD_ROLES = "roles";
+	public static final String FIELD_ROLE_NAME = "name";
+
+	@Value("${microservice.user.find.uri}")
+	private String baseUri;
+
+	public JsonNode findUserByUsername(String username) {
+		URI uri = null;
+		try {
+			uri = new URI(baseUri + username);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		RestTemplate restTemplate = new RestTemplate();
+		return restTemplate.getForObject(uri, JsonNode.class);
+	}
+
+}
