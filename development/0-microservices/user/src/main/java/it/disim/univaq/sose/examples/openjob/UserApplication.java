711affa640c62c75ddb19117872b478b7a2a909d
Gianluca Filippone <filgianluca93@gmail.com>
2024-05-31 20:29:03 +0200

    Bufixes

--- development/0-microservices/user/src/main/java/it/disim/univaq/sose/examples/openjob/UserApplication.java
+++ development/0-microservices/user/src/main/java/it/disim/univaq/sose/examples/openjob/UserApplication.java
@@ -2,7 +2,9 @@ package it.disim.univaq.sose.examples.openjob;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class UserApplication {
 
